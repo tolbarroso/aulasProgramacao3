c7d41786b47dceca2e3ef13e211ab1388c1b8eb2
carol barroso <carol.00000829955@unicap.br>
2022-11-21 13:42:40 +0000

    c

--- actions/TestInstanceInnerClass.java
+++ actions/TestInstanceInnerClass.java
@@ -1,22 +1,25 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class TestInstanceInnerClass() {
-    JFrame frame;
-    button1 = new JButton ("One");
-    button2 = new JButton ("Two");
-    button2 = new JButton ("Two");
-    frame = new JFrame("Test Instance Class");
-    Container cp = frame.getContentPane();
-    cp.add("North", button1);
-    cp.add("Center", button2);
-    Listener lstnr = new Listener();
-    button1.addActionListener(lstnr);
-    button2.addActionListener(lstnr);
-    frame.setSize(175, 150);
-    frame.setVisible(true);
-}
+public class TestInstanceInnerClass {
 
-public static void main(String[] args) {
-    new TestInstanceInnerClass();
+    public void TestInstanceInnerClass() {
+        JFrame frame;
+        button1 = new JButton("One");
+        button2 = new JButton("Two");
+        button2 = new JButton("Two");
+        frame = new JFrame("Test Instance Class");
+        Container cp = frame.getContentPane();
+        cp.add("North", button1);
+        cp.add("Center", button2);
+        Listener lstnr = new Listener();
+        button1.addActionListener(lstnr);
+        button2.addActionListener(lstnr);
+        frame.setSize(175, 150);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new TestInstanceInnerClass();
+    }
 }
